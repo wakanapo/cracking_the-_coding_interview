fce255c303f5915cd8521ac3c771dd753e9a7ba6
野上和加奈 <wakana.tn16@gmail.com>
2017-03-07 19:39:25 +0900

    solve 2.3

---
+++ cpp/chapter2/2_3.cpp
@@ -0,0 +1,60 @@
+/* 
+ * 2_3.cpp
+ * 
+ * Author:   Wakana Nogami <wakana.tn16@gmail.com>
+ * URL:      http://wknp16.tumblr.com               
+ * License:  2-Clause BSD License                    
+ * Created:  2017-03-07                              
+ *
+ *
+ * Copyright (c) 2017, Wakana Nogami
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include "node.h"
+#include <cstdio>
+#include <iostream>
+
+
+void deleteNode(Node *n) {
+  if (n != nullptr && n->next != nullptr)
+    *n = *(n->next);
+}
+
+
+int main() {
+  Node *head = new Node(1);
+  head->appendToTail(3);
+  head->appendToTail(5);
+  head->appendToTail(7);
+  head->appendToTail(9);
+  deleteNode(head->next);
+  Node *n = head;
+  while (n != nullptr) {
+    std::cout << n->data << std::endl;
+    n = n->next;
+  }
+}
