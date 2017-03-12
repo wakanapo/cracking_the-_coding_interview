85e7f32113c81b3a00b9d8d6e3a4a8b89028346b
野上和加奈 <wakana.tn16@gmail.com>
2017-03-12 23:21:49 +0900

    solve 4.5

---
+++ cpp/chapter4/4_5.cpp
@@ -0,0 +1,64 @@
+/* 
+ * 4_5.cpp
+ * 
+ * Author:   Wakana Nogami <wakana.tn16@gmail.com>
+ * URL:      http://wknp16.tumblr.com               
+ * License:  2-Clause BSD License                    
+ * Created:  2017-03-12                              
+ *
+ *
+ * Copyright (c) 2017, Wakana Nogami
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include "tree.h"
+
+#include <iostream>
+
+
+bool checkBST(Node* n, int min, int max) {
+  if (n == nullptr)
+    return true;
+  if (n->value < min || n->value >= max)
+    return false;
+  if (checkBST(n->left, min, n->value) && checkBST(n->right, n->value, max))
+    return true;
+  else
+    return false;
+} 
+
+int main() {
+  Node* tree = new Node(4);
+  tree->left = new Node(2);
+  tree->right = new Node(6);
+  tree->left->left = new Node(1);
+  tree->left->right = new Node(3);
+  if (checkBST(tree, -100, 100))
+    std::cout << "BST: Pass!" << std::endl;
+  tree->right->left = new Node(3);
+  if (!checkBST(tree, -100, 100))
+    std::cout << "NotBST: Pass!" << std::endl;
+}
