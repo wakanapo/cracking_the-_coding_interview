6450d05e9353c2169ef4df63f6b9c6269ab27017
野上和加奈 <wakana.tn16@gmail.com>
2017-03-15 15:13:28 +0900

    solve 4.10

---
+++ cpp/chapter4/4_10.cpp
@@ -0,0 +1,85 @@
+/* 
+ * 4_10.cpp
+ * 
+ * Author:   Wakana Nogami <wakana.tn16@gmail.com>
+ * URL:      http://wknp16.tumblr.com               
+ * License:  2-Clause BSD License                    
+ * Created:  2017-03-15                              
+ *
+ *
+ * Copyright (c) 2017, Wakana Nogami
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include "tree.h"
+
+#include <iostream>
+
+
+bool MatchTree(Node* a, Node* b) {
+  if (a == nullptr || b == nullptr) {
+    if (a == b)
+      return true;
+    else
+      return false;
+  }
+  if (a->value != b->value)
+    return false;
+  else
+    return MatchTree(a->left, b->left) && MatchTree(a->right, b->right);
+}
+
+
+bool SubTree(Node* tree, Node* sub) {
+  if (sub == nullptr)
+    return true;
+  if (tree == nullptr)
+    return false;
+  if (tree->value == sub->value && MatchTree(tree, sub))
+  // １つ目の条件は必須ではない気がするけど実行時間の問題？
+    return true;
+  return SubTree(tree->left, sub) || SubTree(tree->right, sub);
+}
+
+
+int main() {
+  Node* tree = new Node(1);
+  tree->left = new Node(2);
+  tree->left->left = new Node(3);
+  tree->left->right = new Node(4);
+  tree->right = new Node(2);
+  tree->right->left = new Node(3);
+  tree->right->right = new Node(4);
+  tree->right->left->left = new Node(5);
+  Node* sub = new Node(2);
+  sub->left = new Node(3);
+  sub->right = new Node(4);
+  if (SubTree(tree, sub))
+    std::cout << "True: Pass!" << std::endl;
+  tree->left->right = new Node(6);
+  if (!SubTree(tree, sub))
+    std::cout << "False: Pass!" << std::endl;
+}
